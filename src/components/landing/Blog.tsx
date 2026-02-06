6d76712412ddcb6000d4b93a8f875ab0f3224fc7
Aditya <adityagawande94@gmail.com>
2026-02-06 14:50:51 +0530

    Nem2

--- src/components/landing/Blog.tsx
+++ src/components/landing/Blog.tsx
@@ -1,11 +1,11 @@
-import { getPublishedBlogPosts } from '@/lib/blog';
-import { Link } from 'next-view-transitions';
-import React from 'react';
+// import { getPublishedBlogPosts } from '@/lib/blog';
+// import { Link } from 'next-view-transitions';
+// import React from 'react';
 
-import { BlogCard } from '../blog/BlogCard';
-import Container from '../common/Container';
-import SectionHeading from '../common/SectionHeading';
-import { Button } from '../ui/button';
+// import { BlogCard } from '../blog/BlogCard';
+// import Container from '../common/Container';
+// import SectionHeading from '../common/SectionHeading';
+// import { Button } from '../ui/button';
 
 export default function Blog() {
   // const posts = getPublishedBlogPosts();
--- src/components/landing/Projects.tsx
+++ src/components/landing/Projects.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import { projects } from '@/config/Projects';
-import { Link } from 'next-view-transitions';
+// import { Link } from 'next-view-transitions';
 import React from 'react';
 
 import Container from '../common/Container';
 import SectionHeading from '../common/SectionHeading';
 import { ProjectList } from '../projects/ProjectList';
-import { Button } from '../ui/button';
+// import { Button } from '../ui/button';
 
 export default function Projects() {
   return (
